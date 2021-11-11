49d18bb490dc822f0607be8d36f2ad6f25298694
Koh Okada <koh.okada@gmail.com>
2021-11-11 23:45:16 +0900

    JP対応

--- qmk_firmware/keyboards/keyball/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keymaps/default/keymap.c
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "pointing_device.h"
 #include "oledkit.h"
+#include "keymap_jp.h"
 
 enum keymap_layers {
     _QWERTY,
@@ -59,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_BTN2,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_BTN1,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+         KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_BTN1,                         KC_H,    KC_J,    KC_K,    KC_L, JP_SCLN, JP_QUOT,
   //|--------+--------+--------+--------+--------+--------'                    |--------+--------+--------+--------+--------+--------|
-         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_TILD,
+         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, JP_TILD,
   //|--------+--------+--------+--------+--------+-------+--------.            `--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_G_BS,     KC_A_EN,  KC_L_SPC,  KC_CTLTB, KC_ALT_ESC,           KC_ENT,    KC_R_JA,        KC_EXLM, KC_TAB,   KC_ESC
   //`--------+--------'  `--------'  `--------' `--------+--------'              `--------+--------'      `--------+--------+--------'
@@ -69,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_right_ball(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+      KC_EXLM,   JP_AT, KC_HASH,  KC_DLR, KC_PERC, _______,                      JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                      KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,  KC_GRV,
+      _______, _______, _______, _______, _______, _______,                      JP_MINS, JP_PLUS, JP_LCBR, JP_RCBR, JP_PIPE,  JP_GRV,
   //|--------+--------+--------+--------+--------+--------'                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______,                               KC_UNDS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
+      _______, _______, _______, _______, _______,                               JP_UNDS,  JP_EQL, JP_LBRC, JP_RBRC, JP_BSLS, JP_TILD,
   //|--------+--------+--------+--------+--------+-------+--------.            `--------+--------+--------+--------+--------+--------|
       _______, _______,     KC_ENT,   KC_A_DEL,    KC_SPC, _______,                _______, _______,        _______, _______, _______
   //`--------+--------'  `--------'  `--------' `--------+--------'              `--------+--------'      `--------+--------+--------'
@@ -104,6 +105,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 };
+
+// shfft + jp対応
+static bool process_jp_symbols_impl(uint16_t keycode, bool pressed) {
+    if (!pressed) {
+        return true;
+    }
+    uint8_t shift = keyboard_report->mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
+    if (!shift) {
+        return true;
+    }
+    uint16_t s;
+    switch (keycode) {
+        // Replace Shift-Symbols like ANSI for JIS.
+        case JP_2:    s = JP_AT; break;
+        case JP_6:    s = JP_CIRC; break;
+        case JP_7:    s = JP_AMPR; break;
+        case JP_8:    s = JP_ASTR; break;
+        case JP_9:    s = JP_LPRN; break;
+        case JP_0:    s = JP_RPRN; break;
+        case JP_GRV:  s = JP_TILD; break;
+        case JP_EQL:  s = JP_PLUS; break;
+        case JP_MINS: s = JP_UNDS; break;
+        case JP_QUOT: s = JP_DQUO; break;
+        case JP_SCLN: s = JP_COLN; break;
+        default: return true;
+    }
+    unregister_mods(shift);
+    tap_code16(s);
+    register_mods(shift);
+    return false;
+}
+
+bool process_jp_symbols(uint16_t keycode, keyrecord_t *record) {
+    return process_jp_symbols_impl(keycode, record->event.pressed);
+}
+
+void tap_code16jp(uint16_t keycode) {
+    if (process_jp_symbols_impl(keycode, true)) {
+        tap_code16(keycode);
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return process_jp_symbols(keycode, record);
+}
+
+
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
